ea50d7eeec3ebd45cbb61312780343310e544dfe
Christoffer Zakrisson <rustypig91@gmail.com>
2025-11-30 09:35:40 +0100

    Optimizing

--- src/BrowserWindow.cpp
+++ src/BrowserWindow.cpp
@@ -2,12 +2,19 @@
 
 BrowserWindow::BrowserWindow(const QUrl &url) : QMainWindow(nullptr)
 {
+    // Optimize QtWebEngine performance by setting Chromium flags
+    qputenv("QTWEBENGINE_CHROMIUM_FLAGS",
+            "--disable-gpu-vsync --disable-smooth-scrolling --disable-accelerated-2d-canvas "
+            "--disable-background-timer-throttling --disable-renderer-backgrounding "
+            "--disable-features=PaintHolding,SitePerProcess,TouchpadOverscrollHistoryNavigation "
+            "--no-sandbox");
+
     view = new QWebEngineView(this);
     view->setUrl(url);
 
     setCentralWidget(view);
     setWindowState(Qt::WindowFullScreen | Qt::WindowActive);
-    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    // setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
 
     connect(view, &QWebEngineView::loadFinished, this,
             [this](bool ok)
