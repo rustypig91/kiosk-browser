92222248f179888f4bb3cd0116db189764237a37
Christoffer Zakrisson <rustypig91@gmail.com>
2025-12-23 15:48:19 +0100

    Add cookies for Qt6 window

--- src/Qt6BrowserWindow.cpp
+++ src/Qt6BrowserWindow.cpp
@@ -1,4 +1,15 @@
 #include "Qt6BrowserWindow.h"
+#include <QWebEngineProfile>
+#include <QWebEngineCookieStore>
+#include <QNetworkCookie>
+#include <QUrl>
+#include <QStandardPaths>
+#include <QDir>
+#include <qapplication.h>
+
+#ifndef REMOTE_DEBUGGING_PORT
+#define REMOTE_DEBUGGING_PORT "5001"
+#endif
 
 BrowserWindow::BrowserWindow(const QUrl &url) : QMainWindow(nullptr)
 {
@@ -8,13 +19,26 @@ BrowserWindow::BrowserWindow(const QUrl &url) : QMainWindow(nullptr)
             "--disable-background-timer-throttling --disable-renderer-backgrounding "
             "--disable-features=PaintHolding,SitePerProcess,TouchpadOverscrollHistoryNavigation "
             "--no-sandbox");
+#ifdef DEBUG
+    qputenv("QTWEBENGINE_REMOTE_DEBUGGING", "0.0.0.0:" REMOTE_DEBUGGING_PORT);
+#else
+    QApplication::setOverrideCursor(Qt::BlankCursor);
+    QGuiApplication::setOverrideCursor(QCursor(Qt::BlankCursor));
+    setCursor(Qt::BlankCursor);
+#endif
+    QWebEngineProfile *profile = new QWebEngineProfile("kiosk-browser-profile");
+    profile->setPersistentCookiesPolicy(QWebEngineProfile::ForcePersistentCookies);
+    profile->setHttpCacheType(QWebEngineProfile::DiskHttpCache);
 
-    view = new QWebEngineView(this);
+    view = new QWebEngineView(profile);
     view->setUrl(url);
+
+    qDebug("PersistentStoragePath: %s", profile->persistentStoragePath().toUtf8().constData());
+    qDebug("CachePath: %s", profile->cachePath().toUtf8().constData());
 
     setCentralWidget(view);
     setWindowState(Qt::WindowFullScreen | Qt::WindowActive);
-    // setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
 
     connect(view, &QWebEngineView::loadFinished, this,
             [this](bool ok)
